48eb0343ff1e3fef636c5124b098c7ad0ad00902
KronosDP <darrel.danadyaksa19@gmail.com>
2024-02-05 12:10:10 +0700

    Add CreateProductFunctionalTest and update CreateProduct.html

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/CreateProductFunctionalTest.java
@@ -0,0 +1,71 @@
+package id.ac.ui.cs.advprog.eshop.functional;
+
+import id.ac.ui.cs.advprog.eshop.repository.ProductRepository;
+import io.github.bonigarcia.seljup.SeleniumJupiter;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import java.util.List;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@ExtendWith(SeleniumJupiter.class)
+public class CreateProductFunctionalTest {
+
+	@LocalServerPort
+	private int serverPort;
+
+	@Value("${app.baseUrl:http://localhost}")
+	private String testbaseUrl;
+	private String baseUrl;
+
+	@BeforeEach
+	void setUp() {
+		baseUrl = String.format("%s:%d%s", testbaseUrl, serverPort, "/product/create");
+	}
+
+	@Test
+	void pageTitle_isCorrect(ChromeDriver driver) {
+		driver.get(baseUrl);
+		String pageTitle = driver.getTitle();
+		assertEquals("Create New Product", pageTitle);
+	}
+
+	@Test
+	void welcomeMessage_homepage_isCorrect(ChromeDriver driver) throws Exception {
+		driver.get(baseUrl);
+		String welcomeMessage = driver.findElement(By.tagName("h3")).getText();
+		assertEquals("Create New Product", welcomeMessage);
+	}
+
+	@Test
+	void createManyProducts(ChromeDriver driver) {
+		Random random = new Random();
+		int numberOfProducts = 50;
+		for (int i = 0; i < numberOfProducts; i++) {
+			driver.get(baseUrl);
+			WebElement name = driver.findElement(By.name("productName"));
+			String productName = "Product " + random.nextInt(10_000);
+			name.sendKeys(productName);
+
+			WebElement quantity = driver.findElement(By.name("productQuantity"));
+			int productQuantity = 1 + random.nextInt(100);
+			quantity.sendKeys(String.valueOf(productQuantity));
+
+			WebElement submit = driver.findElement(By.id("submitButton"));
+			submit.click();
+		}
+		List<WebElement> productList = driver.findElements(By.tagName("tr"));
+		assertEquals(1 + numberOfProducts, productList.size());
+	}
+
+}
