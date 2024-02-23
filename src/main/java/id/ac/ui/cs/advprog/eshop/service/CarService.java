d842086ef0dd88007074f9e9bb5e18ce1f3e1325
KronosDP <darrel.danadyaksa19@gmail.com>
2024-02-23 20:42:41 +0700

    Tutorial

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarService.java
@@ -0,0 +1,16 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.model.Car;
+import java.util.List;
+
+public interface CarService {
+    public Car create(Car car);
+
+    public List<Car> findAll();
+
+    public Car findById(String id);
+
+    public Car update(String id, Car car);
+
+    public void deleteByCarId(String id);
+}
